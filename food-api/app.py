af785307f4329746865d59646343b4711f8a45ee
Arfan12630 <arfanrahman464@gmail.com>
2022-11-01 18:56:37 -0400

    Connected to postgresql db

--- food-api/app.py
+++ food-api/app.py
@@ -1,8 +1,35 @@
+from crypt import methods, request
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:!@localhost/recipe'
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    
+    def __repr__(self):
+        return f"User: {self.name}"
+
+    def __init__(self,name):
+       self.name = name
+
+def format_event(user):
+    return {
+        "name": user.name,
+        "description":user.description
+    }
+
+@app.route('/user', methods=['POST'])
+def create_event():
+    name = request.json['name']
+    user = User(name)
+    db.session.add(user)
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
