5250192e8b7cf710e0150c187fa6374dd389a9a3
Arfan12630 <arfanrahman464@gmail.com>
2022-11-04 23:40:49 -0400

    Added hover effect to home page

--- food-app/src/views/TheHome.vue
+++ food-app/src/views/TheHome.vue
@@ -24,7 +24,24 @@ body{
   background-size: 110%;
 }
 
-body:hover{
- background-image: url('https://png.pngtree.com/thumb_back/fh260/back_our/20190621/ourmid/pngtree-black-meat-western-food-banner-background-image_194600.jpg');
+body:before,
+body:after{
+   background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  content: '';
+  transition: all 0.3s ease;
+  z-index:1;
+  opacity: 0;
+  
+}
+
+body:hover:before,
+body:hover:after{
+  transform: scale(1);
+  opacity: 1;
 }
 </style>
