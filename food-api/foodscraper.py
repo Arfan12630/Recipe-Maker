00f4efab417adf73e164a7c6f1b8086ad872ff1d
Arfan12630 <arfanrahman464@gmail.com>
2022-11-07 23:53:27 -0500

    Added stuff to UI and web scraped more

--- food-api/foodscraper.py
+++ food-api/foodscraper.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from sqlalchemy.dialects.postgresql import ARRAY
+from app import User, db
+
 
 url = "https://www.simplyrecipes.com/recipes/indian_chicken_biryani/"
 page = requests.get(url).text
@@ -11,6 +14,20 @@ spans = document.find_all('span', class_="meta-text__label")
 timeHeading  = [(span.get_text())for span in spans]
 prepTime = document.find_all('span', class_="meta-text__data")
 timePrep = [(prep.get_text())for prep in prepTime]
+
+if '\n' in timePrep[3]:
+ splitting1 = timePrep[3].replace('\n', ' ')
+ timePrep.pop(3)
+ timePrep.append(splitting1)
+ 
+
 print(timePrep)
 recipe['metadata']['name'] = header
 recipe['metadata']['preps'] = timeHeading
+
+
+# class Info(db.Model):
+#     id=db.Column()
+#     header=db.Column(db.String(50), nullable=False )
+#     timeHeading=db.Column(ARRAY(db.String))
+#     prepTime = db.Column(ARRAY(db.String))
