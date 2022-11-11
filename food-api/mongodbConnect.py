7f09a7abf3e630be9aa3a4c53bf4f5a6698d7d72
Arfan12630 <arfanrahman464@gmail.com>
2022-11-10 22:43:31 -0500

    removed connection string

--- food-api/mongodbConnect.py
+++ food-api/mongodbConnect.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-cluster = MongoClient('mongodb+srv://Arfan12630:yySDLHzQ1vGjgZU8@cluster0.cdrrsj6.mongodb.net/?retryWrites=true&w=majority')
+
 
 db = cluster['recipe']
 
